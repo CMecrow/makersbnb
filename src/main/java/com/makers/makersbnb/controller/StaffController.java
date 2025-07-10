c3519aaae7a0e68f295ff8b5e1d0d1c1b6beb602
Chris Mecrow <mecrow@live.co.uk>
2025-07-10 15:08:39 +0100

    Started work on staff form

--- src/main/java/com/makers/makersbnb/controller/StaffController.java
+++ src/main/java/com/makers/makersbnb/controller/StaffController.java
@@ -6,8 +6,10 @@ import com.makers.makersbnb.repository.SpaceRepository;
 import com.makers.makersbnb.repository.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @RestController
@@ -22,5 +24,19 @@ public class StaffController {
         Iterable<Staff> staff = staffRepository.findAll();
         modelAndView.addObject("staff", staff);
         return modelAndView;
+    }
+
+    @GetMapping("/staff/new")
+    public ModelAndView newStaffForm() {
+        Staff staff = new Staff();
+        ModelAndView newStaffForm = new ModelAndView("/staff/new");
+        newStaffForm.addObject("staff", staff);
+        return newStaffForm;
+    }
+
+    @PostMapping("/staff")
+    public RedirectView create(Staff staff) {
+        staffRepository.save(staff);
+        return new RedirectView("/staff");
     }
 }
